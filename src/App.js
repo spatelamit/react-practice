6de567dccdc5290c22021c95c5e0267f94a85a1c
Amit Patel <spatelamit@gmail.com>
2024-06-02 15:52:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import  Header  from "./Common/Header";
+
+function App() {
+  return (
+    <div>
+      <Header name='amit patel'/>w
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Common/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Header extends React.Component{
+  constructor(){
+
+  }
+  render(){  
+  return (
+      <div>
+        <h1>I am header</h1>
+      </div>
+    );
+  }
+}
+export default Header;
+  
